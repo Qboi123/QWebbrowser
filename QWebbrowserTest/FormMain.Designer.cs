e33e0c405dc47894608b36d7f1715f208b5d37bd
Quinten Jungblut <30625109+Qboi123@users.noreply.github.com>
2021-01-23 03:08:25 +0100

    First release.

---
+++ QWebbrowserTest/FormMain.Designer.cs
@@ -0,0 +1,153 @@
+﻿
+namespace QWebbrowserTest
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            this.browser = new System.Windows.Forms.WebBrowser();
+            this.prevButton = new System.Windows.Forms.Button();
+            this.nextButton = new System.Windows.Forms.Button();
+            this.urlTextBox = new System.Windows.Forms.TextBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.pageLoadProgress = new System.Windows.Forms.ToolStripProgressBar();
+            this.refreshButton = new System.Windows.Forms.Button();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // browser
+            // 
+            this.browser.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.browser.Location = new System.Drawing.Point(0, 24);
+            this.browser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.browser.Name = "browser";
+            this.browser.Size = new System.Drawing.Size(803, 406);
+            this.browser.TabIndex = 0;
+            this.browser.Url = new System.Uri("https://www.google.com", System.UriKind.Absolute);
+            this.browser.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.browser_DocumentCompleted);
+            this.browser.Navigated += new System.Windows.Forms.WebBrowserNavigatedEventHandler(this.BrowserNavigated);
+            this.browser.Navigating += new System.Windows.Forms.WebBrowserNavigatingEventHandler(this.browser_Navigating);
+            this.browser.NewWindow += new System.ComponentModel.CancelEventHandler(this.browser_NewWindow);
+            this.browser.ProgressChanged += new System.Windows.Forms.WebBrowserProgressChangedEventHandler(this.BrowserProgressChanged);
+            // 
+            // prevButton
+            // 
+            this.prevButton.Font = new System.Drawing.Font("Webdings", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.prevButton.Location = new System.Drawing.Point(0, 0);
+            this.prevButton.Name = "prevButton";
+            this.prevButton.Size = new System.Drawing.Size(23, 23);
+            this.prevButton.TabIndex = 1;
+            this.prevButton.Text = "3";
+            this.prevButton.UseVisualStyleBackColor = true;
+            this.prevButton.Click += new System.EventHandler(this.PrevButtonClick);
+            // 
+            // nextButton
+            // 
+            this.nextButton.Font = new System.Drawing.Font("Webdings", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.nextButton.Location = new System.Drawing.Point(24, 0);
+            this.nextButton.Name = "nextButton";
+            this.nextButton.Size = new System.Drawing.Size(23, 23);
+            this.nextButton.TabIndex = 2;
+            this.nextButton.Text = "4";
+            this.nextButton.UseVisualStyleBackColor = true;
+            this.nextButton.Click += new System.EventHandler(this.NextButtonClick);
+            // 
+            // urlTextBox
+            // 
+            this.urlTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.urlTextBox.Location = new System.Drawing.Point(74, 2);
+            this.urlTextBox.Name = "urlTextBox";
+            this.urlTextBox.Size = new System.Drawing.Size(727, 20);
+            this.urlTextBox.TabIndex = 3;
+            this.urlTextBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.UrlTextBoxKeyDown);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.pageLoadProgress});
+            this.statusStrip1.LayoutStyle = System.Windows.Forms.ToolStripLayoutStyle.HorizontalStackWithOverflow;
+            this.statusStrip1.Location = new System.Drawing.Point(0, 430);
+            this.statusStrip1.Margin = new System.Windows.Forms.Padding(0, 6, 0, 0);
+            this.statusStrip1.MaximumSize = new System.Drawing.Size(0, 28);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(803, 22);
+            this.statusStrip1.TabIndex = 4;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // pageLoadProgress
+            // 
+            this.pageLoadProgress.Name = "pageLoadProgress";
+            this.pageLoadProgress.Size = new System.Drawing.Size(100, 16);
+            // 
+            // refreshButton
+            // 
+            this.refreshButton.Font = new System.Drawing.Font("Webdings", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.refreshButton.Location = new System.Drawing.Point(48, 0);
+            this.refreshButton.Name = "refreshButton";
+            this.refreshButton.Size = new System.Drawing.Size(23, 23);
+            this.refreshButton.TabIndex = 5;
+            this.refreshButton.Text = "q";
+            this.refreshButton.UseVisualStyleBackColor = true;
+            this.refreshButton.Click += new System.EventHandler(this.RefreshButtonClick);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(803, 452);
+            this.Controls.Add(this.refreshButton);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.urlTextBox);
+            this.Controls.Add(this.nextButton);
+            this.Controls.Add(this.prevButton);
+            this.Controls.Add(this.browser);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FormMain";
+            this.Text = "Form1";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser browser;
+        private System.Windows.Forms.Button prevButton;
+        private System.Windows.Forms.Button nextButton;
+        private System.Windows.Forms.TextBox urlTextBox;
+        private System.Windows.Forms.ToolStripProgressBar pageLoadProgress;
+        private System.Windows.Forms.Button refreshButton;
+        public System.Windows.Forms.StatusStrip statusStrip1;
+    }
+}
+
---
+++ QWebbrowserTest/FormMain.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QWebbrowserTest
+{
+    public partial class FormMain : Form
+    {
+        private bool isNavigating = false;
+
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void UrlTextBoxKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter || e.KeyCode == Keys.Return)
+            {
+                string url = urlTextBox.Text;
+                try
+                {
+                    browser.Url = new Uri(url);
+                }
+                catch (UriFormatException exc)
+                {
+                    if (!url.Contains("://")) {
+                        try {
+                            browser.Url = new Uri("https://" + url);
+                        }
+                        catch (UriFormatException exception)
+                        {
+                            MessageBox.Show(exception.Message, "Error",
+                            MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show(exc.Message, "Error",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+                e.Handled = true;
+                e.SuppressKeyPress = true;
+            }
+        }
+
+        private void PrevButtonClick(object sender, EventArgs e)
+        {
+            browser.GoBack();
+        }
+
+        private void NextButtonClick(object sender, EventArgs e)
+        {
+            browser.GoForward();
+        }
+
+        private void RefreshButtonClick(object sender, EventArgs e)
+        {
+            if (this.refreshButton.Text == "r")
+            {
+                browser.Stop();
+            } else if (this.refreshButton.Text == "q")
+            {
+                browser.Refresh();
+            }
+        }
+
+        private void BrowserNavigated(object sender, WebBrowserNavigatedEventArgs e)
+        {
+            this.urlTextBox.Text = e.Url.ToString();
+            this.prevButton.Enabled = browser.CanGoBack;
+            this.nextButton.Enabled = browser.CanGoForward;
+        }
+
+        private void BrowserProgressChanged(object sender, WebBrowserProgressChangedEventArgs e)
+        {
+            this.pageLoadProgress.Maximum = 1000;
+            this.pageLoadProgress.Value = (int)(1000 * e.CurrentProgress / e.MaximumProgress);
+            if (e.CurrentProgress == e.MaximumProgress)
+            {
+                pageLoadProgress.Value = 0;
+            }
+        }
+
+        private void browser_NewWindow(object sender, CancelEventArgs e)
+        {
+            new FormMain();
+        }
+
+        private void browser_Navigating(object sender, WebBrowserNavigatingEventArgs e)
+        {
+            this.refreshButton.Text = "r";
+            this.isNavigating = true;
+        }
+
+        private void browser_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            this.isNavigating = false;
+            this.refreshButton.Text = "q";
+        }
+    }
+}
